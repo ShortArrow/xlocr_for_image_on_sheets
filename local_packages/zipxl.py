f7cd040f68564ef5cb2580bccfbb492c50d1eb80
ShortArrow <bamboogeneral@live.jp>
2021-03-02 11:32:15 +0900

    Inheritance

--- local_packages/zipxl.py
+++ local_packages/zipxl.py
@@ -1,30 +1,64 @@
 import zipfile
 
-class Sheet:
+
+class zipxlelement:
+    def __init__(self) -> None:
+        pass
+
+    @property
+    def sheetFolder(self) -> str:
+        return "xl/worksheets/"
+
+    @property
+    def relayFolder(self) -> str:
+        return "xl/worksheets/_rels/"
+
+    @property
+    def mediaFolder(self) -> str:
+        return "xl/media/"
+
+    @property
+    def sheetfileExtension(self) -> str:
+        return ".xml"
+
+
+class Sheet(zipxlelement):
     def __init__(self, name: str) -> None:
         self.name = name
+        self.parent = None
+        self.root = None
 
-class ImageBook:
-    def __init__(self, fileName: str) -> None:
+
+class Image(zipxlelement):
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.parent = None
+        self.root = None
+
+
+class ImageBook(zipxlelement):
+    def __init__(self) -> None:
+        self.Sheets: list[Sheet] = []
+
+    def open(self, fileName: str) -> None:
         self.__zf: zipfile.ZipFile = zipfile.ZipFile(fileName)
-        self.__sheetFolder: str = "xl/worksheets/"
-        self.__relayFolder: str = "xl/worksheets/_rels/"
-        self.__mediaFolder: str = "xl/media/"
-        self.__sheetfileExtension: str = ".xml"
+        self.Sheets = self.__Sheets()
 
     def __del__(self) -> None:
         self.__zf.close()
 
-    def Sheets(self) -> list[Sheet]:
-        res :list[Sheet]=[]
+    def __Sheets(self) -> list[Sheet]:
+        res: list[Sheet] = []
         for item in self.getSheetNames():
             res.append(Sheet(item))
+            res[len(res) - 1].parent = self
+            res[len(res) - 1].root = self
         return res
 
     def getImageList(self) -> list[str]:
         res: list[str] = []
         for name in self.__zf.namelist():
-            if name.startswith(self.__mediaFolder):
+            if name.startswith(self.mediaFolder):
                 res.append(name)
         return res
 
@@ -34,14 +68,14 @@ class ImageBook:
     def getSheetPaths(self) -> list[str]:
         res: list[str] = []
         for name in self.__zf.namelist():
-            if name.startswith(self.__sheetFolder):
-                if not name.startswith(self.__relayFolder):
+            if name.startswith(self.sheetFolder):
+                if not name.startswith(self.relayFolder):
                     res.append(name)
         return res
 
     def getSheetNameFromSheetPath(self, sheet: str) -> str:
-        return sheet.replace(self.__sheetFolder, "").replace(
-            self.__sheetfileExtension, ""
+        return sheet.replace(self.sheetFolder, "").replace(
+            self.sheetfileExtension, ""
         )
 
     def getSheetNames(self) -> list[str]:
@@ -66,9 +100,8 @@ class ImageBook:
     # image/sheet/bookでclassを作って連携させる
 
 
-
 if __name__ == "__main__":
-    xl: ImageBook = ImageBook("./downloads/09390-JGr-Y含む-エクセル数値-210114.xlsx")
-    sheetlist: list[str] = xl.getSheetNames()
-    for item in sheetlist:
-        print(item)
+    xl: ImageBook = ImageBook()
+    xl.open("./downloads/09390-JGr-Y含む-エクセル数値-210114.xlsx")
+    for item in xl.Sheets:
+        print(item.name)
--- test/test_zipxl.py
+++ test/test_zipxl.py
@@ -6,5 +6,5 @@ logger = getLogger('test')
 
 def test_001():
     xl: zipxl.ImageBook = zipxl.ImageBook("./downloads/09390-JGr-Y含む-エクセル数値-210114.xlsx")
-    sheetlist: list[zipxl.Sheet] = xl.Sheets()
+    sheetlist: list[zipxl.Sheet] = xl.__Sheets()
     assert sheetlist[0].name == 'sheet4'
