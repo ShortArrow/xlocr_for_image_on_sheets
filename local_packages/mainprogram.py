f864b5994f251695d219a4ac1adedbc7882182a1
ShortArrow <bamboogeneral@live.jp>
2021-03-09 13:29:28 +0900

    change directory

--- local_packages/mainprogram.py
+++ local_packages/mainprogram.py
@@ -6,15 +6,11 @@ from enum import Enum
 import pytesseract
 from PIL import Image
 import os
+import csv
 
 defaultFileDirctory: str = r"./Downloads/"
-remarkDict: dict = {
-    0: "HOLE CTR",
-    1: "SCRIBE LINE",
-    2: "MEASURING POINT",
-    3: "SHIM",
-    4: "CP",
-}
+remarkColumn: int = 37
+OutputFileDirectory: str = r"./Downloads/output.csv"
 
 
 class remark(Enum):
@@ -25,15 +21,35 @@ class remark(Enum):
     Cp = "CP"
 
 
+class checkDictionary:
+    def __init__(self, mark: remark) -> None:
+        self.__mark = mark
+        self.__has = True
+
+    def setThisMark(self, has: bool) -> None:
+        self.__has = has
+
+    def has(self) -> bool:
+        return self.__has
+
+    def mark(self) -> remark:
+        return self.__mark
+
+    def __str__(self) -> str:
+        return "{" + self.__mark.value + ":" + self.__has + "}"
+
+
 def getLinesFromImage(inputImage: Image.Image) -> list[str]:
-    custom_oem_psm_config = r'-c tessedit_char_whitelist="ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789" --psm 6'
+    custom_oem_psm_config = (
+        r'-c tessedit_char_whitelist="ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789" --psm 6'
+    )
     try:
         # nolook so small image
         if inputImage.size[0] * inputImage.size[1] <= 1600:
             result: str = ""
             return result
         result: str = pytesseract.image_to_string(
-            inputImage,lang="eng", config=custom_oem_psm_config
+            inputImage, lang="eng", config=custom_oem_psm_config
         )
     except TypeError:
         result: str = ""
@@ -47,40 +63,39 @@ def ocr(inputImage: Image.Image) -> list[str]:
 def hasRemark(inputString: str) -> bool:
     for target in remark:
         if target.value in inputString:
-            return True
-    return False
+            return (True, target)
+    return (False, None)
 
 
 if __name__ == "__main__":
     targetXlFileFullPath = fcon.openXlFile(defaultFileDirctory)
     print(targetXlFileFullPath)
     mydata: pylightxl.Database = pylightxl.readxl(targetXlFileFullPath)
-    zipdata: zipxl.ImageBook = zipxl.ImageBook()
-    zipdata.open(targetXlFileFullPath)
+    myBook: zipxl.ImageBook = zipxl.ImageBook()
+    myBook.open(targetXlFileFullPath)
+    resData: list[list] = []
     for targetSheetname in mydata.ws_names:
-        col = mydata.ws(targetSheetname).col(37)
+        buflist: list = []
+        buflist.append(targetSheetname)
+        col = mydata.ws(targetSheetname).col(remarkColumn)
         RemarkCounter = 0
         for cell in col:
             if remark.ScribeLine.value == cell:
                 RemarkCounter += 1
+        buflist.append(RemarkCounter)
         PictureCount = 0
-        for targetSheet in zipdata.Sheets:
+        for targetSheet in myBook.Sheets:
             if targetSheet.displayName == targetSheetname:
-                PictureCount = len(targetSheet.Pictures)
+                buflist.append(len(targetSheet.Pictures))
                 for index, item in enumerate(targetSheet.Pictures):
                     buf: list[str] = getLinesFromImage(item.Image().convert("LA"))
-                    # if targetSheet.displayName=="203-3":
-                        # item.Image().convert("LA").show()
-                    print(str(index)+":", end="")
                     for line in buf:
-                        if hasRemark(line):
-                            print(line, end="")
-                    print()
-                break # displayName is appear only once
-        print(targetSheetname, RemarkCounter, PictureCount)
-
-# HOLE CTR
-# SCRIBE LINE
-# MEASURING POINT
-# SHIM
-# CP
+                        containRemark, hitname = hasRemark(line)
+                        if containRemark:
+                            buflist.append(checkDictionary(hitname))
+                break  # displayName is appear only once
+        resData.append(buflist)
+    with open(OutputFileDirectory, "w") as f:
+        writer = csv.writer(f)
+        for row in resData:
+            writer.writerow(row[0:3] + row[0:3])
